77f16f2c2a9706696da7f455ce7eb98b89b916e5
Vilma Heikkila <vilma.heikkil@hotmail.com>
2022-11-17 15:12:35 +0200

    Change structure

--- test/add.test.js
+++ test/add.test.js
@@ -1,14 +1,14 @@
 // node 12 and up
-//import chai from "chai"
-//import sum from "../sum.js"
+import chai from "chai"
+import add from "../src/add.js"
 // below for node 10
-var chai = require("chai")
-var sum = require ("../sum.js")
+//var chai = require("chai")
+//var sum = require ("../add.js")
 const expect = chai.expect
 
 
 describe("Sum", () => {
  it("adds positive number to positive number", () =>{
-     expect(sum(3,6)).to.equal(9)
+     expect(add(3,6)).to.equal(9)
  });
 })
